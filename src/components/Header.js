c584ca8aa1994b3965b4cb2bfad027a5e2a68b67
jawad hussain <jawwadhussain0@gmail.com>
2022-06-18 04:42:59 -0700

    second

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,36 +2,34 @@ import { React, useEffect, useState } from 'react'
 import styled from "styled-components";
 import axios from "axios";
 import { BsSearch, BsNewspaper } from "react-icons/bs";
-import useFetch from "./useFetch";
 
 
 
 function Header() {
 
     const [input, setInput] = useState('pakistan');
-    const [datas, bussiness, everything] = useFetch(input)
-    // const [datas, setDatas] = useState([]);
-    // const [everything, setEverything] = useState([]);
-    // const [bussiness, setBussiness] = useState([]);
-    // const fetcHeadlines = async () => {
-    //     const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=0e0e6d4d1ee847fa878e9970c951fa69`)
-    //     setDatas(response.data.articles)
-    // }
+    const [datas, setDatas] = useState([]);
+    const [everything, setEverything] = useState([]);
+    const [bussiness, setBussiness] = useState([]);
+    const fetcHeadlines = async () => {
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=0e0e6d4d1ee847fa878e9970c951fa69`)
+        setDatas(response.data.articles)
+    }
 
-    // const fetchEverything = async (url) => {
-    //     const response = await axios.get(url);
-    //     setEverything(response.data.articles)
-    // }
-    // const fetchBussiness = async () => {
-    //     const response = await axios.get('https://newsapi.org/v2/top-headlines/sources?category=business&apiKey=0e0e6d4d1ee847fa878e9970c951fa69')
-    //     setBussiness(response.data.sources);
-    // }
+    const fetchEverything = async (url) => {
+        const response = await axios.get(url);
+        setEverything(response.data.articles)
+    }
+    const fetchBussiness = async () => {
+        const response = await axios.get('https://newsapi.org/v2/top-headlines/sources?category=business&apiKey=0e0e6d4d1ee847fa878e9970c951fa69')
+        setBussiness(response.data.sources);
+    }
 
-    // useEffect(() => {
-    //     fetcHeadlines();
-    //     fetchEverything(`https://newsapi.org/v2/everything?q=${input}&apiKey=0e0e6d4d1ee847fa878e9970c951fa69`)
-    //     fetchBussiness()
-    // }, [input]);
+    useEffect(() => {
+        fetcHeadlines();
+        fetchEverything(`https://newsapi.org/v2/everything?q=${input}&apiKey=0e0e6d4d1ee847fa878e9970c951fa69`)
+        fetchBussiness()
+    }, [input]);
 
     return (
 
