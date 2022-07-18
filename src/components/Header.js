97b16e6f01ec0774e56acc67c70735269f63492a
DESKTOP-A4DPR99\Jawad Hussain <jawwadhussain0@gmail.com>
2022-07-18 05:43:09 -0700

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,34 @@ import { React, useEffect, useState } from 'react'
 import styled from "styled-components";
 import axios from "axios";
 import { BsSearch, BsNewspaper } from "react-icons/bs";
-import useFetch from "./useFetch";
 
 
 
 function Header() {
+
     const [input, setInput] = useState('pakistan');
-    const [datas, everything, bussiness] = useFetch(input);
+    const [datas, setDatas] = useState([]);
+    const [everything, setEverything] = useState([]);
+    const [bussiness, setBussiness] = useState([]);
+    const fetcHeadlines = async () => {
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_API_KEY}`)
+        setDatas(response.data.articles)
+    }
+
+    const fetchEverything = async (url) => {
+        const response = await axios.get(url);
+        setEverything(response.data.articles)
+    }
+    const fetchBussiness = async () => {
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines/sources?category=business&apiKey=${process.env.REACT_APP_API_KEY}`)
+        setBussiness(response.data.sources);
+    }
+
+    useEffect(() => {
+        fetcHeadlines();
+        fetchEverything(`https://newsapi.org/v2/everything?q=${input}&apiKey=${process.env.REACT_APP_API_KEY}`)
+        fetchBussiness()
+    }, [input]);
 
     return (
 
